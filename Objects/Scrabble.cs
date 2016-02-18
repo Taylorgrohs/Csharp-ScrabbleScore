c88379e1e4644c5c2416b150f02a853af87a463b
Taylor <Taylorgrohs@gmail.com>
2016-02-18 08:44:55 -0800

    add passing test two

--- Objects/Scrabble.cs
+++ Objects/Scrabble.cs
@@ -7,7 +7,26 @@ namespace ScrabbleScore
   {
     public int Score(string word)
     {
-    return 1;
+      int score = 0;
+      char[] onePoint = {'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'};
+      char[] twoPoint = {'d', 'g'};
+      char[] threePoint = {'b', 'c', 'm', 'p'};
+      char[] fourPoint = {'f', 'h', 'v', 'w', 'y'};
+      char[] fivePoint = {'k'};
+      char[] eightPoint = {'j', 'x'};
+      char[] tenPoint = {'q', 'z'};
+      char[] wordArray = word.ToLower().ToCharArray();
+      foreach(char i in wordArray)
+      {
+        foreach (char x in onePoint)
+        {
+          if(i == x)
+          {
+            score += 1;
+          }
+        }
+      }
+      return score;
     }
   }
 }
--- Tests/ScrabbleTest.cs
+++ Tests/ScrabbleTest.cs
@@ -11,6 +11,11 @@ namespace ScrabbleScore
       Scrabble newScrabble = new Scrabble();
       Assert.Equal(1, newScrabble.Score("a"));
     }
-
+    [Fact]
+    public void Score_At_equals_2()
+    {
+      Scrabble newScrabble = new Scrabble();
+      Assert.Equal(2, newScrabble.Score("AT"));
+    }
   }
 }
